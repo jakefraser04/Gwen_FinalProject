e99d8356bec640a440b35d7d6ad65bcfc99c9a31
jakefraser04 <135402698+jakefraser04@users.noreply.github.com>
2025-04-24 11:18:54 -0400

    Update ProblemNE.cs

--- Gwen_FinalProject/ProblemNE.cs
+++ Gwen_FinalProject/ProblemNE.cs
@@ -1,4 +1,16 @@
-﻿using System;
+/*
+*File Name: ProblemNE.cs
+*Name: Nariah Edwards
+*email: edwardn5@mail.uc.edu
+*Assignment Number: Final Project
+*Due Date: 04/24/2025
+*Course #/Section: IS3050 Section -- 002
+* Semester / Year: Spring 2025
+*Brief Description of the assignment: This assignment counts as our final project for the class. We're practing working together through GitHub.
+*Brief Description of what this module does: This final project causes students to excute what we've learned throughout the semester.
+*Citations: https://stackoverflow.com/
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;

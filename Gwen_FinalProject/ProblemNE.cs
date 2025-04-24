dbf778fbe4a625b1c41d90835da20f6ef9a316ab
Nariah Edwards <edwardn5@mail.uc.edu>
2025-04-24 11:03:29 -0400

    jake f

--- Gwen_FinalProject/ProblemNE.cs
+++ Gwen_FinalProject/ProblemNE.cs
@@ -5,7 +5,47 @@ using System.Web;
 
 namespace Gwen_FinalProject
 {
-    public class ProblemNE
+    public void SolveSudoku(char[][] board)
     {
+        Solve(board);
+    }
+
+    private bool Solve(char[][] board)
+    {
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (board[row][col] == '.')
+                {
+                    for (char num = '1'; num <= '9'; num++)
+                    {
+                        if (IsValid(board, row, col, num))
+                        {
+                            board[row][col] = num;
+                            if (Solve(board)) return true;
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private bool IsValid(char[][] board, int row, int col, char num)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[row][i] == num || board[i][col] == num)
+                return false;
+
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == num)
+                return false;
+        }
+        return true;
     }
 }
